47743e5674ad0a9b89777fec106e79c546d46806
xpengy <42720167+xpengy@users.noreply.github.com>
2023-04-03 05:15:48 -0400

    remove unnecessary import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { AppShell, Header } from "@mantine/core";
 import {
   HomePage,
   MyRecipesPage,
