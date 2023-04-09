fc1d47b98ce7c9201afcb62efa7558c85b90e461
xpengy <42720167+xpengy@users.noreply.github.com>
2023-04-09 14:07:22 -0400

    b

---
+++ frontend/src/components/Attachments.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { FileButton, Button, Group, Text, List } from "@mantine/core";
+
+export default function Attachments() {
+  const [files, setFiles] = useState([]);
+  return (
+    <>
+      <Group position="center">
+        <FileButton onChange={setFiles} accept="image/png,image/jpeg" multiple>
+          {(props) => <Button {...props}>Upload image</Button>}
+        </FileButton>
+      </Group>
+
+      {files.length > 0 && (
+        <Text size="sm" mt="sm">
+          Picked files:
+        </Text>
+      )}
+
+      <List size="sm" mt={5} withPadding>
+        {files.map((file, index) => (
+          <List.Item key={index}>{file.name}</List.Item>
+        ))}
+      </List>
+    </>
+  );
+}
