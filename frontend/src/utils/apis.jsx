15d49898e039c729eae9de6333bdc43f557279d7
xpengy <42720167+xpengy@users.noreply.github.com>
2023-04-11 11:57:51 -0400

    temp api update

--- frontend/src/utils/apis.jsx
+++ frontend/src/utils/apis.jsx
@@ -15,3 +15,49 @@ export const getRecipesAPI = async () => {
     console.error(error);
   }
 };
+
+export const addIngredientAPI = async (formValues) => {
+  console.log(formValues);
+
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:8000/recipes/add-ingredient-to-recipe/",
+      {
+        name: formValues.recipeName,
+        serving: formValues.serving,
+        prep_time: formValues.prepTime,
+        cooking_time: formValues.cookingTime,
+      }
+    );
+
+    if (response.status === 200) {
+    } else {
+      console.log(response.data);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createBaseIngredientAPI = async (formValues) => {
+  console.log(formValues);
+
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:8000/recipes/create-base-ingredient/",
+      {
+        name: formValues.recipeName,
+        serving: formValues.serving,
+        prep_time: formValues.prepTime,
+        cooking_time: formValues.cookingTime,
+      }
+    );
+
+    if (response.status === 200) {
+    } else {
+      console.log(response.data);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
