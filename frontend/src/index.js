f7d73801ead003fc9b526e3fb6039751455087cb
xpengy <42720167+xpengy@users.noreply.github.com>
2023-04-13 12:52:31 -0400

    import axios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { MantineProvider } from "@mantine/core";
+import axios from "axios";
 
 function setAuthToken() {
   const authToken = localStorage.getItem("authToken");
